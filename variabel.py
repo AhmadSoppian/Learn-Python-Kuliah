ab8fb1ebcc2b7281959c6e5385af275f0e145c06
Ahmad Soppian <ahmadsoppian3@gmail.com>
2025-09-23 23:08:17 +0700

    Day 1 Belajar Python membuat file dan memahami apa itu variabel dan tipe data dalam python

---
+++ variabel.py
@@ -0,0 +1,35 @@
+# Menurut platform pembelajaran Python seperti Real Python, Hero Vired, dan Launch School, 
+# variabel dalam Python adalah nama simbolis yang berfungsi sebagai wadah untuk menyimpan nilai atau objek di memori.
+
+# tipe data di python
+
+# string yang ditandai dengan "" atau ''
+# string bisa menyimpan karakter atau teks bahkan simbol dan emoji
+Nama = 'ahmad soppian'
+prodi = "sistem informasi"
+
+# menampilkan tipe data pada variabel
+print(type(Nama))
+print(type(prodi))
+
+# int atau interger
+# tipe data yang berisi atau menyimpan sebuah angka
+NIM = 19251391
+
+# menampilkan tipe data pada variabel
+print(type(NIM))
+
+# float
+# tipe data yang berisi atau menyimpan angka desimal atau pecahan
+Tinggi = 166.2
+
+# menampilkan tipe data pada variabel
+print(type(Tinggi))
+
+#  bolean hanya untuk 2 nilai yaitu True dan False
+SudahMenikah = False
+Mahasiswa = True
+
+# menampilkan tipe data pada variabel
+print(type(SudahMenikah))
+print(type(Mahasiswa))
